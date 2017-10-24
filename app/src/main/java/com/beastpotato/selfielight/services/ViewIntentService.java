663be2d796d0fe31c7a775c4704e228d49eb4917
Oleksiy Martynov <3martynov@gmail.com>
2017-10-24 07:58:12 -0400

    scale + translation logic

--- app/src/main/java/com/beastpotato/selfielight/services/ViewIntentService.java
+++ app/src/main/java/com/beastpotato/selfielight/services/ViewIntentService.java
@@ -11,14 +11,13 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.beastpotato.selfielight.R;
-
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 
 public class ViewIntentService extends IntentService {
@@ -74,25 +73,22 @@ public class ViewIntentService extends IntentService {
         if(onTopView==null) {
             LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
             onTopView = inflater.inflate(R.layout.layout_selfie_light,null);
-            onTopView.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    return true;
-                }
-            });
         }
         try {
             Display display = wm.getDefaultDisplay();
             Point point = new Point();
             display.getSize(point);
             wm.addView(onTopView, getParams(point.x>point.y?point.y:point.x));
-            PhotoViewAttacher photoViewAttacher =  new PhotoViewAttacher(onTopView.findViewById(R.id.selfie_light_image_view));
-            photoViewAttacher.setMinimumScale(0.5f);
-            photoViewAttacher.update();
+            onTopView.setOnTouchListener(new ViewTouchListener(wm));
+//            PhotoViewAttacher photoViewAttacher =  new PhotoViewAttacher(onTopView.findViewById(R.id.selfie_light_image_view));
+//            photoViewAttacher.setMinimumScale(0.5f);
+//            photoViewAttacher.update();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+
+
     private void makeToast(String text){
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
@@ -118,5 +114,95 @@ public class ViewIntentService extends IntentService {
         else
             params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
         return params;
+    }
+
+    private class ViewTouchListener implements View.OnTouchListener {
+        private int lastAction;
+        private int initialX;
+        private int initialY;
+        private float initialTouchX;
+        private float initialTouchY;
+        private WindowManager windowManager;
+        private ScaleGestureDetector scaleGestureDetector;
+        private OnPinchListener pinchListener;
+        public ViewTouchListener(WindowManager windowManager) {
+            this.windowManager = windowManager;
+            pinchListener = new OnPinchListener();
+            scaleGestureDetector = new ScaleGestureDetector(getApplicationContext(), pinchListener);
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            WindowManager.LayoutParams params = (WindowManager.LayoutParams)v.getLayoutParams();
+            pinchListener.setView(v);
+            pinchListener.setWindowManager(windowManager);
+            scaleGestureDetector.onTouchEvent(event);
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+
+                    initialX = params.x;
+                    initialY = params.y;
+
+                    initialTouchX = event.getRawX();
+                    initialTouchY = event.getRawY();
+
+                    lastAction = event.getAction();
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    if (lastAction == MotionEvent.ACTION_DOWN) {
+                        //todo handle click
+                    }
+                    lastAction = event.getAction();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+
+                    params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                    params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                    windowManager.updateViewLayout(v, params);
+                    lastAction = event.getAction();
+                    return true;
+            }
+            return false;
+        }
+    }
+    private class OnPinchListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        float startingSpan;
+        float endSpan;
+        float startFocusX;
+        float startFocusY;
+        private View view;
+        private WindowManager windowManager;
+
+        public OnPinchListener() {
+        }
+
+        public void setView(View view) {
+            this.view = view;
+        }
+
+        public void setWindowManager(WindowManager windowManager) {
+            this.windowManager = windowManager;
+        }
+
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            startingSpan = detector.getCurrentSpan();
+            startFocusX = detector.getFocusX();
+            startFocusY = detector.getFocusY();
+            return true;
+        }
+
+        public boolean onScale(ScaleGestureDetector detector) {
+            float delta = detector.getCurrentSpan()/startingSpan;
+            WindowManager.LayoutParams params = (WindowManager.LayoutParams)view.getLayoutParams();
+            params.width *=delta;
+            params.height *=delta;
+            windowManager.updateViewLayout(view,params);
+            return true;
+        }
+
+        public void onScaleEnd(ScaleGestureDetector detector) {
+
+        }
     }
 }
